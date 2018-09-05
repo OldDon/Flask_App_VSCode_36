0ca2e5835c355d14587228a7db31761c4bb86e40
olddon <davey.elliott@gmail.com>
2018-09-05 21:15:03 +0100

    Made modification trial in home() - No change

--- app.py
+++ app.py
@@ -7,13 +7,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return 'Hello, Flask!'
+    return 'Hello, Flask! This is the home page:)'
 
 @app.route("/hello/<name>")
 def hello_there(name):
     now = datetime.now()
     formatted_now = now.strftime("%A, %d, %B, %Y at %X")
+    content = "<strong>Hello there, " + name + "!</strong> It's " + formatted_now
 
+    return render_template("hello_there.html", title='Hello, Flask', content = content)
 """ Commented out this block of code from original 'hello_there.html'
     # Filter the name argument to letters only using regular expressions. URL arguments
     # can contain arbitrary text, so we restrict to safe characters only.
@@ -27,7 +29,3 @@ def hello_there(name):
     content = "Hello there, " + clean_name + "! It's " + formatted_now
     return content
 """
-
-content = "<strong>Hello there, " + name + "!</strong> It's " + formatted_now
-
-return render_template("hello_there.html", title='Hello, Flask', content = content)
